f61a0bdb96ea9231ed52c8e6a6c21dc6db88f24e
Daniel Dinev <azsumbg@gmail.com>
2026-08-07 19:51:26 +0300

    Add project files.

---
+++ Ghosts attack/Ghosts attack.cpp
@@ -0,0 +1,235 @@
+﻿#include "framework.h"
+#include "Ghosts attack.h"
+#include <mmsystem.h>
+#include <d2d1.h>
+#include <dwrite.h>
+#include "ErrH.h"
+#include "FCheck.h"
+#include "D2BMPLOADER.h"
+#include "gifresizer.h"
+#include "platsrv.h"
+#include <chrono>
+
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "d2d1.lib")
+#pragma comment(lib, "dwrite.lib")
+#pragma comment(lib, "errh.lib")
+#pragma comment(lib, "fcheck.lib")
+#pragma comment(lib, "d2bmploader.lib")
+#pragma comment(lib, "gifresizer.lib")
+#pragma comment(lib, "platsrv.lib")
+
+constexpr wchar_t bWinClassName[]{ L"Ghosts" };
+constexpr char tmp_file[]{ ".\\res\\data\\temp.dat" };
+constexpr wchar_t Ltmp_file[]{ L".\\res\\data\\temp.dat" };
+constexpr wchar_t snd_file[]{ L".\\res\\snd\\main.wav" };
+constexpr wchar_t record_file[]{ L".\\res\\data\\record.dat" };
+constexpr wchar_t help_file[]{ L".\\res\\data\\help.dat" };
+constexpr wchar_t save_file[]{ L".\\res\\data\\save.dat" };
+
+constexpr int mNew{ 1001 };
+constexpr int mSpeed{ 1002 };
+constexpr int mExit{ 1003 };
+constexpr int mLoad{ 1004 };
+constexpr int mSave{ 1005 };
+constexpr int mHoF{ 1006 };
+
+constexpr int no_record{ 2001 };
+constexpr int first_record{ 2002 };
+constexpr int record{ 2003 };
+
+WNDCLASS bWinClass{};
+HINSTANCE bIns{ nullptr };
+HICON bIcon{ nullptr };
+HCURSOR bCursor{ nullptr };
+HCURSOR outCursor{ nullptr };
+HDC PaintDC{ nullptr };
+PAINTSTRUCT bPaint{};
+HMENU bBar{ nullptr };
+HMENU bMain{ nullptr };
+HMENU bStore{ nullptr };
+HWND bHwnd{ nullptr };
+MSG bMsg{ nullptr };
+BOOL bRet{ 0 };
+UINT bTimer{ 0 };
+
+POINT cur_pos{};
+
+wchar_t current_player[16]{ L"TARLYO" };
+
+D2D1_RECT_F FullScreen{ 0, 0, scr_width, scr_height };
+D2D1_RECT_F GameScreen{ 0, 50.0f, sky, ground };
+
+D2D1_RECT_F b1Rect{ 30.0f, 10.0f, scr_width / 3.0f - 30.0f, 40.0f };
+D2D1_RECT_F b2Rect{ scr_width / 3.0f + 30.0f, 10.0f, scr_width * 2.0f / 3.0f - 30.0f, 40.0f };
+D2D1_RECT_F b3Rect{ scr_width * 2.0f / 3.0f + 30.0f, 10.0f, scr_width - 30.0f, 40.0f };
+
+D2D1_RECT_F b1TxtRect{ 50.0f, 15.0f, scr_width / 3.0f - 30.0f, 40.0f };
+D2D1_RECT_F b2TxtRect{ scr_width / 3.0f + 50.0f, 15.0f, scr_width * 2.0f / 3.0f - 30.0f, 40.0f };
+D2D1_RECT_F b3TxtRect{ scr_width * 2.0f / 3.0f + 40.0f, 15.0f, scr_width - 30.0f, 40.0f };
+
+bool pause{ false };
+bool sound{ true };
+bool in_client{ true };
+bool show_help{ false };
+bool level_skipped{ false };
+bool name_set{ false };
+bool b1Hglt{ false };
+bool b2Hglt{ false };
+bool b3Hglt{ false };
+
+float x_scale{ 0 };
+float y_scale{ 0 };
+
+float level = 1.0f;
+
+int score = 0;
+int mins = 0;
+int secs = 0;
+
+ID2D1Factory* iFactory{ nullptr };
+ID2D1HwndRenderTarget* Draw{ nullptr };
+
+ID2D1RadialGradientBrush* b1BckgBrush{ nullptr };
+ID2D1RadialGradientBrush* b2BckgBrush{ nullptr };
+ID2D1RadialGradientBrush* b3BckgBrush{ nullptr };
+
+ID2D1SolidColorBrush* statBrush{ nullptr };
+ID2D1SolidColorBrush* txtBrush{ nullptr };
+ID2D1SolidColorBrush* inactBrush{ nullptr };
+ID2D1SolidColorBrush* hgltBrush{ nullptr };
+
+IDWriteFactory* iWriteFactory{ nullptr };
+IDWriteTextFormat* nrmFormat{ nullptr };
+IDWriteTextFormat* midFormat{ nullptr };
+IDWriteTextFormat* bigFormat{ nullptr };
+
+ID2D1Bitmap* bmpShot{ nullptr };
+ID2D1Bitmap* bmpArmor{ nullptr };
+ID2D1Bitmap* bmpChest{ nullptr };
+ID2D1Bitmap* bmpLife{ nullptr };
+ID2D1Bitmap* bmpGun{ nullptr };
+
+ID2D1Bitmap* bmpLogoLevel{ nullptr };
+ID2D1Bitmap* bmpLogoWin{ nullptr };
+ID2D1Bitmap* bmpLogoLoose{ nullptr };
+ID2D1Bitmap* bmpLogoRecord{ nullptr };
+
+ID2D1Bitmap* bmpBoulder{ nullptr };
+ID2D1Bitmap* bmpRock{ nullptr };
+ID2D1Bitmap* bmpTree1{ nullptr };
+ID2D1Bitmap* bmpTree2{ nullptr };
+ID2D1Bitmap* bmpTree3{ nullptr };
+
+ID2D1Bitmap* bmpDirt{ nullptr };
+ID2D1Bitmap* bmpGrass{ nullptr };
+ID2D1Bitmap* bmpGrassDirt{ nullptr };
+ID2D1Bitmap* bmpGrassFB{ nullptr };
+ID2D1Bitmap* bmpGrassFR{ nullptr };
+
+ID2D1Bitmap* bmpIntro[106]{ nullptr };
+
+ID2D1Bitmap* bmpHeroHor[2]{ nullptr };
+ID2D1Bitmap* bmpHeroVerD[2]{ nullptr };
+ID2D1Bitmap* bmpHeroVerU[2]{ nullptr };
+
+ID2D1Bitmap* bmpBrain[202]{ nullptr };
+ID2D1Bitmap* bmpDervish[68]{ nullptr };
+ID2D1Bitmap* bmpGhost[223]{ nullptr };
+ID2D1Bitmap* bmpSoul[120]{ nullptr };
+
+//////////////////////////////////////////////////////
+
+template<typename T>concept HasRelease = requires(T var)
+{
+	var.Release();
+};
+template<HasRelease T>bool FreeMem(T** var)
+{
+	if (*var)
+	{
+		(*var)->Release();
+		(*var) = nullptr;
+		return true;
+	}
+
+	return false;
+};
+void LogErr(const wchar_t* what_happened)
+{
+	std::wofstream err(L".\\res\\data\\error.log", std::ios::app);
+	err << what_happened << L" Time stamp: " << std::chrono::system_clock::now() << std::endl;
+	err.close();
+}
+void ClearResources()
+{
+	if (!FreeMem(&iFactory))LogErr(L"Error releasing D2D1 iFactory !");
+	if (!FreeMem(&Draw))LogErr(L"Error releasing D2D1 HwndRenderTarget !");
+	if (!FreeMem(&b1BckgBrush))LogErr(L"Error releasing D2D1 b1BckgBrush !");
+	if (!FreeMem(&b2BckgBrush))LogErr(L"Error releasing D2D1 b2BckgBrush !");
+	if (!FreeMem(&b3BckgBrush))LogErr(L"Error releasing D2D1 b3BckgBrush !");
+	if (!FreeMem(&statBrush))LogErr(L"Error releasing D2D1 statBrush !");
+	if (!FreeMem(&txtBrush))LogErr(L"Error releasing D2D1 txtBrush !");
+	if (!FreeMem(&hgltBrush))LogErr(L"Error releasing D2D1 hgltBrush !");
+	if (!FreeMem(&inactBrush))LogErr(L"Error releasing D2D1 inactBrush !");
+
+	if (!FreeMem(&iWriteFactory))LogErr(L"Error releasing D2D1 iWriteFactory !");
+	if (!FreeMem(&nrmFormat))LogErr(L"Error releasing D2D1 nrmFormat !");
+	if (!FreeMem(&midFormat))LogErr(L"Error releasing D2D1 midFormat !");
+	if (!FreeMem(&bigFormat))LogErr(L"Error releasing D2D1 bigFormat !");
+
+	if (!FreeMem(&bmpShot))LogErr(L"Error releasing D2D1 bmpShot !");
+	if (!FreeMem(&bmpArmor))LogErr(L"Error releasing D2D1 bmpArmor !");
+	if (!FreeMem(&bmpChest))LogErr(L"Error releasing D2D1 bmpChest !");
+	if (!FreeMem(&bmpLife))LogErr(L"Error releasing D2D1 bmpLife !");
+	if (!FreeMem(&bmpGun))LogErr(L"Error releasing D2D1 bmpGun !");
+
+	if (!FreeMem(&bmpLogoLevel))LogErr(L"Error releasing D2D1 bmpLogoLevel !");
+	if (!FreeMem(&bmpLogoWin))LogErr(L"Error releasing D2D1 bmpLogoWin !");
+	if (!FreeMem(&bmpLogoLoose))LogErr(L"Error releasing D2D1 bmpLogoLoose !");
+	if (!FreeMem(&bmpLogoRecord))LogErr(L"Error releasing D2D1 bmpLogoRecord !");
+
+	if (!FreeMem(&bmpBoulder))LogErr(L"Error releasing D2D1 bmpBoulder !");
+	if (!FreeMem(&bmpRock))LogErr(L"Error releasing D2D1 bmpRock !");
+	if (!FreeMem(&bmpTree1))LogErr(L"Error releasing D2D1 bmpTree1 !");
+	if (!FreeMem(&bmpTree2))LogErr(L"Error releasing D2D1 bmpTree2 !");
+	if (!FreeMem(&bmpTree3))LogErr(L"Error releasing D2D1 bmpTree3 !");
+
+	if (!FreeMem(&bmpDirt))LogErr(L"Error releasing D2D1 bmpDirt !");
+	if (!FreeMem(&bmpGrassDirt))LogErr(L"Error releasing D2D1 bmpGrassDirt !");
+	if (!FreeMem(&bmpGrass))LogErr(L"Error releasing D2D1 bmpGrass !");
+	if (!FreeMem(&bmpGrassFB))LogErr(L"Error releasing D2D1 bmpGrassFB !");
+	if (!FreeMem(&bmpGrassFR))LogErr(L"Error releasing D2D1 bmpGrassFR !");
+
+	for (int i = 0; i < 106; ++i)if (!FreeMem(&bmpIntro[i]))LogErr(L"Error releasing D2D1 bmpIntro !");
+
+	for (int i = 0; i < 2; ++i)if (!FreeMem(&bmpHeroHor[i]))LogErr(L"Error releasing D2D1 bmpHeroHor !");
+	for (int i = 0; i < 2; ++i)if (!FreeMem(&bmpHeroVerD[i]))LogErr(L"Error releasing D2D1 bmpHeroVerD !");
+	for (int i = 0; i < 2; ++i)if (!FreeMem(&bmpHeroVerU[i]))LogErr(L"Error releasing D2D1 bmpHeroVerU !");
+
+	for (int i = 0; i < 202; ++i)if (!FreeMem(&bmpBrain[i]))LogErr(L"Error releasing D2D1 bmpBrain !");
+	for (int i = 0; i < 68; ++i)if (!FreeMem(&bmpDervish[i]))LogErr(L"Error releasing D2D1 bmpDervish !");
+	for (int i = 0; i < 223; ++i)if (!FreeMem(&bmpGhost[i]))LogErr(L"Error releasing D2D1 bmpGhost !");
+	for (int i = 0; i < 120; ++i)if (!FreeMem(&bmpSoul[i]))LogErr(L"Error releasing D2D1 bmpSoul !");
+}
+
+
+
+
+
+
+
+
+
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+	bIns = hInstance;
+
+
+
+
+
+
+    return (int) msg.wParam;
+}
---
+++ Ghosts attack/platSrv.h
@@ -0,0 +1,630 @@
+﻿#pragma once
+
+#ifdef PLATSRV_EXPORTS 
+#define PLATSRV_API __declspec(dllexport)
+#else
+#define PLATSRV_API __declspec(dllimport)
+#endif
+
+#include <random>
+#include <d2d1.h>
+
+#pragma comment(lib, "d2d1.lib")
+
+constexpr float scr_width{ 1000.0f };
+constexpr float scr_height{ 800.0f };
+
+constexpr float sky{ 50.0f };
+constexpr float ground{ 750.0f };
+
+constexpr float ground_speed{ 1.0f };
+
+constexpr float LINE_EPSILON{ 0.001f };
+
+constexpr D2D1_RECT_F FULL_SCREEN{ 0, 0, scr_width, scr_height };
+constexpr D2D1_RECT_F GAME_SCREEN{ 0, sky, scr_width, ground };
+
+constexpr int ERR_PTR{ 5001 };
+constexpr int ERR_INDEX{ 5002 };
+constexpr int ERR_PARAM{ 5003 };
+constexpr int ERR_UNK{ 5004 };
+
+constexpr int MAX_FIELD_ROWS{ 16 };
+constexpr int MAX_FIELD_COLS{ 22 };
+
+enum class dirs { up = 0, down = 1, left = 2, right = 3, up_left = 4, up_right = 5, down_left = 6, down_right = 7, stop = 8 };
+enum class evils { brain = 0, dervish = 1, ghost = 2, soul = 3 };
+enum class assets { armor = 0, life = 1, shot = 2 };
+enum class tiles { grass = 0, grass_blue = 1, grass_red = 2, grass_dirt = 3, dirt = 4 };
+enum class obstacles { tree1 = 0, tree2 = 1, tree3 = 2, boulder = 3, rock = 4};
+enum class actions { move = 0, attack = 1, patrol = 2 };
+
+namespace dll
+{
+	struct PLATSRV_API TILE
+	{
+		tiles type{ tiles::grass };
+		
+		D2D1_RECT_F rect{};
+
+		float delay{ 0 };
+	};
+	
+	struct PLATSRV_API GRID_COORD
+	{
+		int row{ 0 };
+		int col{ 0 };
+	};
+
+	struct PLATSRV_API FADING
+	{
+		assets type = assets::armor;
+
+		D2D1_RECT_F rect{};
+
+		float opacity()
+		{
+			--delay;
+			if (delay <= 0)
+			{
+				_opacity -= 0.1f;
+				delay = 20;
+			}
+
+			return _opacity;
+		}
+
+	private:
+		int delay = 20;
+		float _opacity{ 1.0f };
+	};
+
+	class PLATSRV_API EXCEPTION
+	{
+	private:
+		int _type{ 0 };
+
+	public:
+		EXCEPTION(int what);
+
+		const wchar_t* eGet()const;
+	};
+
+	template<typename T> class BAG
+	{
+	private:
+		T* mPtr{ nullptr };
+		size_t max_size{ 1 };
+		size_t next_pos{ 0 };
+
+	public:
+
+		BAG()
+		{
+			mPtr = reinterpret_cast<T*>(calloc(max_size, sizeof(T)));
+			if (!mPtr)throw EXCEPTION(ERR_PTR);
+		}
+		BAG(size_t lenght)
+		{
+			max_size = lenght;
+			mPtr = reinterpret_cast<T*>(calloc(lenght, sizeof(T)));
+
+			if (!mPtr)throw EXCEPTION(ERR_PTR);
+		}
+		BAG(BAG& other)
+		{
+			if (!other.mPtr)throw EXCEPTION(ERR_PARAM);
+			else
+			{
+				max_size = other.max_size;
+				next_pos = other.next_pos;
+
+				mPtr = reinterpret_cast<T*>(calloc(max_size, sizeof(T)));
+				
+				if (!mPtr)throw EXCEPTION(ERR_PARAM);
+				else if (next_pos > 0)for (size_t i = 0; i < next_pos; ++i)mPtr[i] = other.mPtr[i];
+			}
+		}
+		BAG(BAG&& other)
+		{
+			if (!other.mPtr)throw EXCEPTION(ERR_PTR);
+			else
+			{
+				max_size = other.max_size;
+				next_pos = other.next_pos;
+				mPtr = other.mPtr;
+
+				other.mPtr = nullptr;
+			}
+		}
+
+		~BAG()
+		{
+			free(mPtr);
+		}
+
+		bool empty() const
+		{
+			return(next_pos == 0);
+		}
+		size_t size() const
+		{
+			return next_pos;
+		}
+		size_t capacity() const
+		{
+			return max_size;
+		}
+
+		BAG& operator = (BAG& other)
+		{
+			if (!other.mPtr)throw EXCEPTION(ERR_PARAM);
+			else
+			{
+				free(mPtr);
+
+				max_size = other.max_size;
+				next_pos = other.next_pos;
+
+				mPtr = reinterpret_cast<T*>(calloc(max_size, sizeof(T)));
+
+				if (!mPtr)throw EXCEPTION(ERR_PARAM);
+				else if (next_pos > 0)for (size_t i = 0; i < next_pos; ++i)mPtr[i] = other.mPtr[i];
+			}
+
+			return *this;
+		}
+		BAG& operator = (BAG&& other)
+		{
+			if (!other.mPtr)throw EXCEPTION(ERR_PARAM);
+			else
+			{
+				free(mPtr);
+
+				max_size = other.max_size;
+				next_pos = other.next_pos;
+				mPtr = other.mPtr;
+
+				other.mPtr = nullptr;
+			}
+
+			return *this;
+		}
+
+		T& operator[](size_t index)
+		{
+			if (!mPtr)throw EXCEPTION(ERR_PTR);
+			else if (index < 0 || index >= next_pos)throw EXCEPTION(ERR_PARAM);
+			
+			return mPtr[index];
+		}
+		T& front()
+		{
+			if (!mPtr)throw EXCEPTION(ERR_PTR);
+			else if (next_pos == 0)throw EXCEPTION(ERR_PARAM);
+
+			return *mPtr;
+		}
+		T& back()
+		{
+			if (!mPtr)throw EXCEPTION(ERR_PTR);
+			else if (next_pos == 0)throw EXCEPTION(ERR_PARAM);
+
+			return mPtr[next_pos - 1];
+		}
+
+		void push_back(T element)
+		{
+			if (!mPtr)throw EXCEPTION(ERR_PTR);
+			else
+			{
+				if (next_pos + 1 <= max_size)
+				{
+					mPtr[next_pos] = element;
+					++next_pos;
+				}
+				else
+				{
+					++max_size;
+
+					mPtr = reinterpret_cast<T*>(realloc(mPtr, max_size * sizeof(T)));
+					
+					if (!mPtr)throw EXCEPTION(ERR_PTR);
+					else mPtr[next_pos] = element;
+					
+					++next_pos;
+				}
+			}
+		}
+		void push_back(T* element)
+		{
+			if (!mPtr)throw EXCEPTION(ERR_PTR);
+			else
+			{
+				if (next_pos + 1 <= max_size)
+				{
+					mPtr[next_pos] = *element;
+					++next_pos;
+				}
+				else
+				{
+					++max_size;
+
+					mPtr = reinterpret_cast<T*>(realloc(mPtr, max_size * sizeof(T)));
+
+					if (!mPtr)throw EXCEPTION(ERR_PTR);
+					else mPtr[next_pos] = *element;
+
+					++next_pos;
+				}
+			}
+		}
+
+		void push_front(T element)
+		{
+			if (!mPtr)throw EXCEPTION(ERR_PTR);
+			else
+			{
+				if (next_pos + 1 <= max_size)
+				{
+					for (size_t ind = next_pos; ind > 0; --ind)mPtr[ind] = mPtr[ind - 1];
+					*mPtr = element;
+					++next_pos;
+				}
+				else
+				{
+					++max_size;
+
+					mPtr = reinterpret_cast<T*>(realloc(mPtr, max_size * sizeof(T)));
+
+					if (!mPtr)throw EXCEPTION(ERR_PTR);
+					else
+					{
+						for (size_t ind = next_pos; ind > 0; --ind)mPtr[ind] = mPtr[ind - 1];
+						*mPtr = element;
+						++next_pos;
+					}
+				}
+			}
+		}
+		void push_front(T* element)
+		{
+			if (!mPtr)throw EXCEPTION(ERR_PTR);
+			else
+			{
+				if (next_pos + 1 <= max_size)
+				{
+					for (size_t ind = next_pos; ind > 0; --ind)mPtr[ind] = mPtr[ind - 1];
+					*mPtr = *element;
+					++next_pos;
+				}
+				else
+				{
+					++max_size;
+
+					mPtr = reinterpret_cast<T*>(realloc(mPtr, max_size * sizeof(T)));
+
+					if (!mPtr)throw EXCEPTION(ERR_PTR);
+					else
+					{
+						for (size_t ind = next_pos; ind > 0; --ind)mPtr[ind] = mPtr[ind - 1];
+						*mPtr = *element;
+						++next_pos;
+					}
+				}
+			}
+		}
+
+		void erase(size_t index)
+		{
+			if (!mPtr) throw EXCEPTION(ERR_PTR);
+			else
+			{
+				if (index < 0 || index >= next_pos)throw EXCEPTION(ERR_PARAM);
+
+				for (size_t ind = index; ind < next_pos - 1; ++ind)mPtr[ind] = mPtr[ind + 1];
+				--next_pos;
+			}
+		}
+		void clear()
+		{
+			max_size = 1;
+			next_pos = 0;
+
+			free(mPtr);
+
+			mPtr = reinterpret_cast<T*>(calloc(max_size, sizeof(T)));
+		}
+
+		void insert(size_t index, T element)
+		{
+			if (!mPtr)throw EXCEPTION(ERR_PTR);
+			else
+			{
+				if (index < 0 || index >= next_pos)throw EXCEPTION(ERR_PARAM);
+
+				if (next_pos + 1 <= max_size)
+				{
+					for (size_t ind = next_pos; ind > index; --ind)mPtr[ind] = mPtr[ind - 1];
+					mPtr[index] = element;
+					++next_pos;
+				}
+				else
+				{
+					++max_size;
+					mPtr = reinterpret_cast<T*>(realloc(mPtr, max_size * sizeof(T)));
+					if (!mPtr)throw EXCEPTION(ERR_PTR);
+					else
+					{
+						for (size_t ind = next_pos; ind > index; --ind)mPtr[ind] = mPtr[ind - 1];
+						mPtr[index] = element;
+						++next_pos;
+					}
+				}
+			}
+		}
+		void insert(size_t index, T* element)
+		{
+			if (!mPtr)throw EXCEPTION(ERR_PTR);
+			else
+			{
+				if (index < 0 || index >= next_pos)throw EXCEPTION(ERR_PARAM);
+
+				if (next_pos + 1 <= max_size)
+				{
+					for (size_t ind = next_pos; ind > index; --ind)mPtr[ind] = mPtr[ind - 1];
+					mPtr[index] = *element;
+					++next_pos;
+				}
+				else
+				{
+					++max_size;
+					mPtr = reinterpret_cast<T*>(realloc(mPtr, max_size * sizeof(T)));
+					if (!mPtr)throw EXCEPTION(ERR_PTR);
+					else
+					{
+						for (size_t ind = next_pos; ind > index; --ind)mPtr[ind] = mPtr[ind - 1];
+						mPtr[index] = *element;
+						++next_pos;
+					}
+				}
+			}
+		}
+	};
+
+	class PLATSRV_API RANDIT
+	{
+	private:
+		std::mt19937* twister{ nullptr };
+
+	public:
+		RANDIT();
+		~RANDIT();
+
+		int operator()(int min, int max);
+		float operator()(float min, float max);
+	};
+
+	class PLATSRV_API PROTON
+	{
+	protected:
+		float _width{ 0 };
+		float _height{ 0 };
+
+		D2D1_RECT_F _rect{};
+
+	public:
+		D2D1_POINT_2F start{};
+		D2D1_POINT_2F end{};
+		D2D1_POINT_2F center{};
+
+		float x_rad{ 0 };
+		float y_rad{ 0 };
+
+		PROTON();
+		PROTON(float sx, float sy);
+		PROTON(float sx, float sy, float first_width, float first_height);
+		PROTON(D2D1_POINT_2F start_point, float first_width, float first_height);
+
+		virtual ~PROTON() {};
+
+		float get_width()const;
+		float get_height()const;
+
+		D2D1_RECT_F get_rect()const;
+
+		void new_width(float new_width);
+		void new_height(float new_height);
+		void new_dims(float new_width, float new_height);
+
+		void set_edges();
+	};
+
+	class PLATSRV_API FIELD
+	{
+	private:
+		TILE FieldArray[MAX_FIELD_ROWS][MAX_FIELD_COLS]{};
+		
+		float _speed{ ground_speed };
+
+		RANDIT randerer{};
+
+		void add_tiles(dirs towhere);
+
+	public:
+
+		FIELD();
+
+		void move(float gear, dirs dir);
+
+		TILE get_tile(int row, int col) const;
+
+		GRID_COORD get_coord(float x, float y) const;
+		GRID_COORD get_coord(D2D1_POINT_2F point) const;
+	};
+
+	class PLATSRV_API HERO :public PROTON
+	{
+	private:
+		float _speed{ 3.0f };
+
+		int frame = 0;
+		int max_frames = 1;
+		int frame_delay = 30;
+		int max_frame_delay = 3;
+
+		bool hor_dir = false;
+		bool ver_dir = false;
+
+		float move_sx{ 0 };
+		float move_ex{ 0 };
+		float move_sy{ 0 };
+		float move_ey{ 0 };
+
+		float slope{ 0 };
+		float intercept{ 0 };
+
+		HERO(float _sx, float _sy);
+
+	public:
+		float angle{ 0 };
+
+		int lifes = 150;
+		int damage = 20;
+		int armor = 1;
+
+		void set_path(float targ_x, float targ_y);
+
+		float get_target_x()const;
+		float get_target_y()const;
+
+		int get_frame();
+
+		void move(float gear);
+		
+		void set_view_angle();
+
+		void Release();
+
+		static HERO* create(float sx, float sy);
+	};
+
+	class PLATSRV_API SHOT :public PROTON
+	{
+	private:
+		float speed = { 8.0f };
+
+		bool hor_dir{ false };
+		bool ver_dir{ false };
+
+		float move_sx{ 0 };
+		float move_ex{ 0 };
+		float move_sy{ 0 };
+		float move_ey{ 0 };
+
+		float slope{ 0 };
+		float intercept{ 0 };
+
+		SHOT(float _sx, float _sy, float _ex, float _ey);
+
+	public:
+
+		int damage{ 0 };
+
+		bool move(float gear);
+
+		void Release();
+
+		SHOT* create(float sx, float sy, float ex, float ey);
+	};
+
+	class PLATSRV_API OBSTACLE :public PROTON
+	{
+	private:
+		float _speed{ ground_speed };
+
+		OBSTACLE(obstacles _type, float _sx, float _sy);
+
+	public:
+		obstacles type{ obstacles::tree1 };
+
+		bool move(float gear, dirs dir);
+		void Release();
+
+		static OBSTACLE* create(obstacles my_type, float sx, float sy);
+	};
+
+	class PLATSRV_API EVILS :public PROTON
+	{
+	private:
+
+		RANDIT _randerer{};
+		int frame{ 0 };
+		int max_frames{ 0 };
+		int frame_delay{ 0 };
+		int max_frame_delay{ 0 };
+
+		int attack_delay{ 0 };
+		int max_attack_delay{ 0 };
+
+		float speed{ 0 };
+
+		bool hor_dir = false;
+		bool ver_dir = false;
+
+		float move_sx{ 0 };
+		float move_ex{ 0 };
+		float move_sy{ 0 };
+		float move_ey{ 0 };
+
+		float slope{ 0 };
+		float intercept{ 0 };
+
+		float final_target_x{ 0 };
+		float final_target_y{ 0 };
+		bool obstacle_on_path{ false };
+
+		float view_range{ 0 };
+
+		EVILS(evils what, float _sx, float _sy);
+
+	public:
+		evils type{};
+		actions action{ actions::patrol };
+		
+		int lifes{ 0 };
+		int damage{ 0 };
+		int armor{ 0 };
+
+		void move(float gear);
+		void set_path(float targ_x, float targ_y);
+
+		float get_target_x()const;
+		float get_target_y()const;
+
+		int get_frame();
+		int attack();
+
+		void Release();
+
+		actions AIMove(BAG<D2D1_POINT_2F>& AssetsCenters, BAG<D2D1_RECT_F>& ObstBag, D2D1_POINT_2F hero_center);
+
+		static EVILS* create(evils what, float sx, float sy);
+	};
+
+	// FUNCTIONS *******************************
+
+	bool PLATSRV_API Intersect(D2D1_RECT_F first, D2D1_RECT_F second);
+	bool PLATSRV_API Intersect(D2D1_POINT_2F first_center, D2D1_POINT_2F second_center, float first_xrad, float second_xrad,
+		float first_yrad, float second_yrad);
+
+	float PLATSRV_API Distance(D2D1_POINT_2F first, D2D1_POINT_2F second);
+	
+	void PLATSRV_API Sort(BAG<D2D1_POINT_2F>& Bag, D2D1_POINT_2F ref);
+
+	void PLATSRV_API Sort(BAG<D2D1_RECT_F>& Bag, D2D1_POINT_2F ref);
+
+	bool PLATSRV_API Point_on_line(D2D1_POINT_2F start_line, D2D1_POINT_2F end_line, D2D1_POINT_2F check_point);
+
+	bool PLATSRV_API Line_box_intersect(D2D1_RECT_F Box, float slope, float intercept);
+}
